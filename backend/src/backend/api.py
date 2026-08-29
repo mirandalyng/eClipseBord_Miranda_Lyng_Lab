5448eaa37426db23479595f9305c3462db06b0fb
Miranda Lyng <lyngmiranda@gmail.com>
2026-08-29 15:43:00 +0200

    Add solar and lunar datasets into the data processing, import into api.py

---
+++ backend/src/backend/api.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI
+
+# importing both dataframes of the solar and lunar datasets
+from backend.data_processing import solar_df, lunar_df
--- backend/src/backend/data_processing.py
+++ backend/src/backend/data_processing.py
@@ -1,4 +1,7 @@
 import pandas as pd
 from backend.constants import DATA_PATH
 
-df = pd.read_csv(DATA_PATH /)
+# creating dataframes for both datasets
+solar_df = pd.read_csv(DATA_PATH / "solar.csv")
+
+lunar_df = pd.read_csv(DATA_PATH / "lunar.csv")
