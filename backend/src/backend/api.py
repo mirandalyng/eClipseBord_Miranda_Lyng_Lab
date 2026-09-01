4b482d6a560278bf0dbef6c5313c655bacdede83
Miranda Lyng <lyngmiranda@gmail.com>
2026-09-01 09:41:28 +0200

    fastAPI: endpoint for lunar and solar. Fill missing values with 0 in the solar dataset.

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -2,3 +2,18 @@ from fastapi import FastAPI
 
 # importing both dataframes of the solar and lunar datasets
 from backend.data_processing import solar_df, lunar_df
+
+app = FastAPI()
+
+# dict for the datasets
+DATASETS = {
+    "solar": solar_df,
+    "lunar": lunar_df,
+}
+
+
+@app.get("/eclipse/{data}")
+async def show_data(data: str):
+    df = DATASETS.get(data)
+    # had to set the limit lower for the data to be able to load on my computer
+    return df.head(20).to_dict(orient="records")
--- backend/src/backend/data_processing.py
+++ backend/src/backend/data_processing.py
@@ -5,3 +5,7 @@ from backend.constants import DATA_PATH
 solar_df = pd.read_csv(DATA_PATH / "solar.csv")
 
 lunar_df = pd.read_csv(DATA_PATH / "lunar.csv")
+
+# fill missing values with 0
+solar_df["Path Width (km)"] = solar_df["Path Width (km)"].fillna(0)
+solar_df["Central Duration"] = solar_df["Central Duration"].fillna(0)
