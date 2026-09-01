8969087c7759c165089f688fbc648490297531c2
Miranda Lyng <lyngmiranda@gmail.com>
2026-09-01 11:03:34 +0200

    dashboard.py, create function for main, dataset, get_data

--- backend/src/backend/data_processing.py
+++ backend/src/backend/data_processing.py
@@ -6,6 +6,11 @@ solar_df = pd.read_csv(DATA_PATH / "solar.csv")
 
 lunar_df = pd.read_csv(DATA_PATH / "lunar.csv")
 
-# fill missing values with 0
-solar_df["Path Width (km)"] = solar_df["Path Width (km)"].fillna(0)
-solar_df["Central Duration"] = solar_df["Central Duration"].fillna(0)
+# fill missing values with missing
+solar_df["Path Width (km)"] = solar_df["Path Width (km)"].fillna("missing")
+solar_df["Central Duration"] = solar_df["Central Duration"].fillna("missing")
+
+lunar_df["Partial Eclipse Duration (m)"] = lunar_df["Partial Eclipse Duration (m)"].fillna(
+    "missing")
+lunar_df[",Total Eclipse Duration (m)"] = lunar_df["Total Eclipse Duration (m)"].fillna(
+    "missing")
--- frontend/src/frontend/dashboard.py
+++ frontend/src/frontend/dashboard.py
@@ -1,11 +1,43 @@
 import streamlit as st
 import httpx
+from backend.data_processing import solar_df, lunar_df
+import pandas as pd
+from pathlib import Path
+
+BASE_URL = "http://127.0.0.1:8000"
+IMAGES_DIR = Path(__file__).parents[3] / "images"
 
 
-BASE_URL = "http: // 127.0.0.1: 8000/eclipse/"
+def get_data(DATASET):
+    response = httpx.get(f"{BASE_URL}/eclipse/{DATASET}")
+    data = response.json()
+    return pd.DataFrame(data)
+
+
+def show_dataset(df):
+    st.markdown("### Eclipse Data")
+    st.dataframe(df)
 
 
 def main():
-    st.markdown("# Eclipse data")
+    st.markdown("# About Eclipses")
 
-    stats = httpx.get
+    st.info("An eclipse is an awe-inspiring celestial event that drastically changes "
+            "the appearance of the two biggest objects we see in our sky: our Sun and Moon. "
+            "On Earth, people can experience solar and lunar eclipses when Earth, the Moon, and the Sun line up. "
+            "Safety is the number one priority when viewing a solar eclipse.  \n Source: NASA")
+
+    st.markdown("### Solar vs Lunar Eclipse")
+
+    st.image(IMAGES_DIR/"eclipse.jpg")
+    st.markdown("### Choose Dataset")
+
+    DATASET = st.selectbox("", ["solar", "lunar"])
+
+    df = get_data(DATASET)
+
+    show_dataset(df)
+
+
+if __name__ == "__main__":
+    main()
